a2fe40676315585f95b009ab81e071c3ca501e38
kristof.rado@gmail.com <kristof.rado@gmail.com>
2017-02-21 09:22:30 +0100

    flask-dojo setup.

---
+++ dojo/models.py
@@ -0,0 +1,14 @@
+from dojo.connectdatabase import ConnectDatabase
+from peewee import *
+
+
+class Entries(Model):
+    story_title = CharField()
+    user_story = TextField()
+    accepting_criteria = TextField()
+    business_value = IntegerField()
+    estimation = DecimalField()
+    status = CharField()
+
+    class Meta:
+        database = ConnectDatabase.db
